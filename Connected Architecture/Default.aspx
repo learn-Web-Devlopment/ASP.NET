822453dcef7c74f12fc04c34dacceace801b852f
darshan2141 <ubhadiyadarshan@gmail.com>
2023-08-12 15:20:44 +0530

    Upload

---
+++ Connected Architecture/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server" placeholder="Enter name"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server" placeholder="Enter email"></asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox3" runat="server" placeholder="Enter Message"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+    &nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Delete" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Select All" />
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Select" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Connected Architecture/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""|DataDirectory|\Database.mdf"";Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("insert into registration (name,email,msg) values ('"+TextBox1.Text+"','"+TextBox2.Text+"','"+TextBox3.Text+"')",con);
+        cmd.ExecuteNonQuery();
+        Label1.Text = "Data Inserted";
+        con.Close();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("update registration set email='"+TextBox2.Text+"' ,msg='"+TextBox3.Text+"' where name='"+TextBox1.Text+"'", con);
+        cmd.ExecuteNonQuery();
+        Label1.Text = "Data Updated";
+        con.Close();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("delete from registration where name='" + TextBox1.Text + "'", con);
+        cmd.ExecuteNonQuery();
+        Label1.Text = "Data Deleted";
+        con.Close();
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from registration ", con);
+        SqlDataReader dr = cmd.ExecuteReader();
+        GridView1.DataSource = dr;
+        GridView1.DataBind();
+        con.Close();
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from registration where name='"+TextBox1.Text+"' ", con);
+        SqlDataReader dr = cmd.ExecuteReader();
+        GridView1.DataSource = dr;
+        GridView1.DataBind();
+        con.Close();
+    }
+}
