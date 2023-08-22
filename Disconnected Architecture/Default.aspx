53e9813aa5534aa6d1e6c69cadaa62e2e71b32d0
darshan2141 <ubhadiyadarshan@gmail.com>
2023-08-22 23:26:03 +0530

    Disconnected

---
+++ Disconnected Architecture/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Font-Size="30px" Text="Disconnected Architecture"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Email"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Comment"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox3" runat="server" TextMode="MultiLine"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add New" Width="142px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Delete" Width="142px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Update" Width="142px" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Disconnected Architecture/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""|DataDirectory|\Database.mdf"";Integrated Security=True");
+    public static DataSet ds = new DataSet();
+    public static SqlDataAdapter da;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+
+        display();
+    }
+
+    public void display()
+    {
+        ds.Clear();
+        string str = "select * from feedback";
+        da = new SqlDataAdapter(str, con);
+        da.Fill(ds, "feedback");
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+       DataRow dr = ds.Tables["feedback"].NewRow();
+
+        dr["name"] = TextBox1.Text;
+        dr["email"] = TextBox2.Text;
+        dr["message"] = TextBox3.Text;
+
+        ds.Tables["feedback"].Rows.Add(dr);
+
+        SqlCommandBuilder cmdr = new SqlCommandBuilder(da);
+        da.Update(ds.Tables["feedback"]);
+        display();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string filter = "Id=" + Convert.ToInt32(TextBox1.Text);
+        DataRow[] dr = ds.Tables["feedback"].Select(filter);
+        dr[0].Delete();
+        SqlCommandBuilder cmdr = new SqlCommandBuilder(da);
+        da.Update(ds.Tables["feedback"]);
+        display();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        string str = "select * from feedback";
+        da = new SqlDataAdapter(str,con);
+        DataRow[] dr = ds.Tables[0].Select("Id=" + Convert.ToInt32(TextBox1.Text));
+
+        dr[0]["email"] = TextBox2.Text;
+        dr[0]["message"] = TextBox3.Text;
+        SqlCommandBuilder cmdr = new SqlCommandBuilder(da);
+        da.Update(ds.Tables["feedback"]);
+        display();
+
+    }
+}
