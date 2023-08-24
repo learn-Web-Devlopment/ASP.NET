383d0033f7b275f0f48f1e26167ed0f3c59169e1
darshan2141 <ubhadiyadarshan@gmail.com>
2023-08-24 14:50:36 +0530

    program 2

---
+++ Program 3/Program 3/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""|DataDirectory|\Database.mdf"";Integrated Security=True");
+
+        con.Open();
+
+        SqlCommand cmd = new SqlCommand("select * from users",con); 
+
+        SqlDataReader dr = cmd.ExecuteReader();
+
+        GridView1.DataSource = dr;
+        GridView1.DataBind();
+    }
+}
