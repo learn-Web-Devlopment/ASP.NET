e2385bfc58bfce02c6754203fe06a7bb71a73256
darshan2141 <ubhadiyadarshan@gmail.com>
2023-08-24 15:20:12 +0530

    Program 5

---
+++ Program 5/Program 5/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs"
+Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head runat="server">
+    <title></title>
+  </head>
+  <body>
+    <form id="form1" runat="server">
+      <div>
+        <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Email"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Message"></asp:Label>
+        <br />
+        <asp:TextBox
+          ID="TextBox3"
+          runat="server"
+          TextMode="MultiLine"
+        ></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button
+          ID="Button1"
+          runat="server"
+          OnClick="Button1_Click"
+          Text="Insert"
+        />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server"> </asp:GridView>
+      </div>
+    </form>
+  </body>
+</html>
---
+++ Program 5/Program 5/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""|DataDirectory|\Database.mdf"";Integrated Security=True");
+    public static DataSet ds = new DataSet();
+    public static SqlDataAdapter da ;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        display();
+    }
+
+    public void display()
+    {
+        ds.Clear();
+        string str = "select * from users";
+        da = new SqlDataAdapter(str, con);
+        da.Fill(ds, "users");
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        DataRow dr = ds.Tables["users"].NewRow();
+
+        dr["name"] = TextBox1.Text;
+        dr["email"] = TextBox2.Text;
+        dr["msg"] = TextBox3.Text;
+
+        ds.Tables["users"].Rows.Add(dr);
+
+        SqlCommandBuilder cmdr = new SqlCommandBuilder(da);
+        da.Update(ds.Tables["users"]);
+        display();
+    }
+}
